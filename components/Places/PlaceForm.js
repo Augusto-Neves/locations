986216dca44125d567b627063a97b7a3b31ca514
Augusto Neves <augustoneves@frwk.com.br>
2023-08-11 12:15:32 -0300

    lessons 191 and 192

---
+++ components/Places/PlaceForm.js
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+export function PlaceForm() {
+  return (
+    <View>
+        <Text>Place Form goes here</Text>
+    </View>
+  );
+}
+
---
+++ components/UI/IconButton.js
@@ -0,0 +1,19 @@
+import { TouchableOpacity, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+export function IconButton({ color, icon, onPress, size }) {
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.button} activeOpacity={0.7}>
+      <Ionicons name={icon} color={color} size={size} />
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 8,
+    margin: 4,
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+});
